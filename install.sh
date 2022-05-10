bf3d2b771e0100b148a1086f7c49bbc86351ab37
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-05-10 19:43:50 +0100

    fix main installer url

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing cloudflared"
-sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
+sudo /usr/bin/curl -sf https://raw.githubusercontent.com/gentleseal/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /bin/chmod +x /config/scripts/post-config.d/cloudflared-setup.sh
 sed -i "s/TOKEN/$1/g" /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /config/scripts/post-config.d/cloudflared-setup.sh pull
