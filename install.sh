4d585f9d180512946d499fe97662776bffeba4a4
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-05-10 19:44:21 +0100

    sudo sed

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -e
 echo "Installing cloudflared"
 sudo /usr/bin/curl -sf https://raw.githubusercontent.com/gentleseal/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /bin/chmod +x /config/scripts/post-config.d/cloudflared-setup.sh
-sed -i "s/TOKEN/$1/g" /config/scripts/post-config.d/cloudflared-setup.sh
+sudo sed -i "s/TOKEN/$1/g" /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /config/scripts/post-config.d/cloudflared-setup.sh pull
