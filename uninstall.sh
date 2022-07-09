3534847c7e7d88461e48565ed9198b9dd7144124
Artur Prets <ap01421@surrey.ac.uk>
2022-07-09 14:28:19 +0300

    feat: update uninstall for new init

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,5 @@
 source /opt/vyatta/etc/functions/script-template
 
 # Stop cloudflared and remove files
-sudo /etc/init.d/cloudflared stop
-sudo /opt/cloudflared/cloudflared service uninstall
+sudo pkill -f "/opt/cloudflared/cloudflared"
 sudo rm -rf /etc/cloudflared /opt/cloudflared /config/scripts/post-config.d/cloudflared-setup.sh /var/log/cloudflared*
