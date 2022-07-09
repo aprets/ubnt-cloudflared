533b3a036fecd66344efb0a4c62d0916af66ac51
Artur Prets <ap01421@surrey.ac.uk>
2022-07-09 14:38:13 +0300

    fix: non conflicting func name

--- cloudflared-setup.sh
+++ cloudflared-setup.sh
@@ -3,14 +3,15 @@ source /opt/vyatta/etc/functions/script-template
 
 mkdir -p /opt/cloudflared
 
-run () {
+run_cloudflared ()
+{
 	nohup /opt/cloudflared/cloudflared --no-autoupdate tunnel run --token TOKEN >> /var/log/cloudflared 2>&1 &
 }
 
 if [ ! -f /opt/cloudflared/cloudflared ] || [ "$1" = "pull" ]; then
 	/usr/bin/curl -sf https://raw.githubusercontent.com/gentleseal/ubnt-cloudflared/master/cloudflared --output /opt/cloudflared/cloudflared
 	/bin/chmod +x /opt/cloudflared/cloudflared
-	run
+	run_cloudflared
 else
-	run
+	run_cloudflared
 fi
