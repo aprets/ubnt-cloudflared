749b69389c35ada3872dbe7c8517d7d8325527f6
Artur Prets <ap01421@surrey.ac.uk>
2022-07-09 14:45:11 +0300

    feat: allow pasting the token later

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/vbash
 set -e
 
+if [ -z "$1" ]; then
+	echo "No token argument supplied"
+	echo "Please enter the cloudflare tunnel token bellow:"
+	read TOKEN
+else
+	TOKEN=$1
+fi
+
 echo "Installing cloudflared"
 sudo /usr/bin/curl -sf https://raw.githubusercontent.com/gentleseal/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /bin/chmod +x /config/scripts/post-config.d/cloudflared-setup.sh
