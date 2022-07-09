0cb98ceff7a0f8122168d3e83b15c8aadf11c134
Artur Prets <ap01421@surrey.ac.uk>
2022-07-09 12:26:14 +0300

    refactor: change init of process
    with hopes of stability

--- cloudflared-setup.sh
+++ cloudflared-setup.sh
@@ -6,8 +6,6 @@ mkdir -p /opt/cloudflared
 
 if [ ! -f /opt/cloudflared/cloudflared ] || [ "$1" = "pull" ]; then
 	sudo /usr/bin/curl -sf https://raw.githubusercontent.com/gentleseal/ubnt-cloudflared/master/cloudflared --output /opt/cloudflared/cloudflared
-
 	/bin/chmod +x /opt/cloudflared/cloudflared
-	/opt/cloudflared/cloudflared service install TOKEN
-	/etc/init.d/cloudflared restart
+	/opt/cloudflared/cloudflared --no-autoupdate tunnel run --token TOKEN >> /var/log/cloudflared 2>&1 &
 fi
